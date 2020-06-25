1f48c5e4afeaa166c29ba514a1c87f8243817c1e
jessicabohn74 <47541659+jessicabohn74@users.noreply.github.com>
2020-06-25 15:37:45 -0300

    Add files via upload
    quatro operações matematicas

---
+++ quatroOperacoes.js
@@ -0,0 +1,16 @@
+function somar (num1, num2){
+    return num1 + num2
+}
+function subtrair (num1, num2){
+    return num1 - num2
+}
+function multiplicar (num1, num2){
+    return num1 * num2
+}
+function dividir (num1, num2){
+    return num1 / num2
+}
+console.log(somar(10, 50))
+console.log(subtrair(10, 50))
+console.log(multiplicar(10, 50))
+console.log(dividir(10, 50))
