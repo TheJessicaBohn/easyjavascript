adb30e27ded2d3a244d38457abc17d2f6b15db35
jessicabohn74 <47541659+jessicabohn74@users.noreply.github.com>
2020-06-26 11:41:56 -0300

    Add files via upload

---
+++ verdadeiroFalso.js
@@ -0,0 +1,14 @@
+const verificaseMaior = function (teste){
+    if (teste % 3 == 0 ){
+    console.log('verdadeiro')
+}else{
+    console.log ('falso')
+}
+    
+}
+
+verificaseMaior(9)
+verificaseMaior(4)
+verificaseMaior(15)
+verificaseMaior(18)
+verificaseMaior(8)
