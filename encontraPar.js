992b1b74904bb15a1726aa8bd6754b414756b448
jessicabohn74 <47541659+jessicabohn74@users.noreply.github.com>
2020-06-29 09:13:34 -0300

    Add files via upload

---
+++ encontraPar.js
@@ -0,0 +1,10 @@
+
+ var encontraPar = 0; 
+for (let i = 1; i<=100; i++){
+   if(i%2 === 0){
+    console.log('Numeros pares ' + i)
+    encontraPar++;
+}
+var par1 = encontraPar + 0;
+}
+console.log('quantidade de numeros pares ' + par1)
