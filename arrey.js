8e29a618721b7fd6047ee4a774316684843c0db2
Jéssica Bohn <47541659+jessicabohn74@users.noreply.github.com>
2020-07-06 11:12:04 -0300

    Add files via upload

---
+++ arrey.js
@@ -0,0 +1,14 @@
+const arraydivisao = [
+    function divisao (dividendo,divisor){
+    return dividendo / divisor
+    },
+    function resto (dividendo,divisor){
+    return dividendo % divisor;
+},
+
+]
+
+console.log(arraydivisao[0](8,3))
+console.log(arraydivisao[1](8,3))
+console.log(arraydivisao[0](1,2))
+console.log(arraydivisao[1](1,2))
