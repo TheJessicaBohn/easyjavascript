7a91b5a7cd97e71e3055ba3f8915b164c24c5c55
Jéssica Bohn <47541659+TheJessicaBohn@users.noreply.github.com>
2020-07-19 16:22:42 -0300

    Add files via upload

---
+++ semana.js
@@ -0,0 +1,30 @@
+const semana = function (dia){
+    switch (dia) {             
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        		console.log('dia util');
+        		break
+        case 1:
+        case 7:
+       		    console.log('final de semana')
+         		break
+
+      	default:
+       		console.log('dia inválido')
+         	}
+
+    }
+    semana(1)
+    semana(2)
+    semana(3)
+    semana(4)
+    semana(5)
+    semana(6)
+    semana(7)
+    semana(8)
+    
+      
+
